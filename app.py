dbbf9f2925f74055386158982be474d17409d466
hocinedev <hocinedev4@gmail.com>
2024-06-07 11:25:25 +0100

    updates

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-@app.route('/')
-def upload_form():
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'File successfully uploaded'
     return render_template('upload.html')
 
 @app.route('/upload', methods=['POST'])
